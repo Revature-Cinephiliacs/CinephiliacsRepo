88d1fd205cf89c62aa3a101bbe02a352ef1195a8
Roger Plyler <rwplyler@yahoo.com>
2021-05-01 19:24:36 -0400

    remove admin permission form

--- src/app/admintools/admintools.component.ts
+++ src/app/admintools/admintools.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../admin.service';
 import {ReportedItem } from "../models";
 import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-admintools',
@@ -9,13 +10,29 @@ import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
   styleUrls: ['./admintools.component.scss']
 })
 export class AdmintoolsComponent implements OnInit {
-
+  //adds Group Permissions
   form: FormGroup;
+  //removes Admin Permissions
+  removeform:FormGroup;
   tickets: ReportedItem[];
+  userId: string;
+  username: string;
+  authModel: any;
   
-  constructor(private fb: FormBuilder,private admin: AdminService) {}
+  constructor(private auth: AuthService, private fb: FormBuilder,private admin: AdminService) {}
 
   ngOnInit(): void {
+
+    this.auth.authModel$.subscribe(reply => {
+      console.log();
+      
+      this.userId = reply.userid;
+      this.username = reply.username;
+      console.log(this.userId);
+      console.log(this.authModel);
+    });
+
+    this.auth.isAdmin$.subscribe(response => console.log("Response" + response));
     this.admin.getReports().then(result =>{
       this.tickets.push(result);
     });
@@ -23,15 +40,30 @@ export class AdmintoolsComponent implements OnInit {
       UserName: [''],
       Rights: ['']
     });
+    this.removeform = this.fb.group({
+      UserName: [''],
+      Rights: ['']
+    });
   }
 
-  submitUserChanges() {
+  submitUserChanges(username,rights) {
     if(this.form.value.UserName != ''){
       console.log(this.form.value);
       if(this.form.value.Rights = "admin"){
         //Add AdminRights
       }else if(this.form.value.Rights = "moderator"){
         //Add ModeratorRights
+      }
+    }
+  }
+
+  submitRemoveChanges(username,rights){
+    if(this.removeform.value.UserName != ''){
+      console.log(this.form.value);
+      if(this.form.value.Rights = "admin"){
+        //remove AdminRights
+      }else if(this.form.value.Rights = "moderator"){
+        //remove ModeratorRights
       }
     }
   }
