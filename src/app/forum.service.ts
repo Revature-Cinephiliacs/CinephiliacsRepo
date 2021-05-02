f96998042f7d692fb8885aace82998b9be78bce8
sagarluitel01 <sagar.luitel@revature.net>
2021-05-01 22:16:05 -0600

    Discussion Ready for testing & styling

--- src/app/forum.service.ts
+++ src/app/forum.service.ts
@@ -15,7 +15,7 @@ export class ForumService {
    }
 
   getDiscussion(movieId: String) {
-    return this.http.get<Discussion[]>(this.forumsUrl + "forum/discussions/" + movieId);
+    return this.http.get<Discussion[]>(this.forumsUrl + "forum/discussionsmovie/" + movieId);
   }
 
   //Function that will make a call to the Forum API discussions/movieid endpoint
@@ -24,9 +24,21 @@ export class ForumService {
     return this.http.get<Discussion[]>( this.forumsUrl + "forum/discussions/"+movieId+"/"+page+"/"+sortingOrder);
   }
 
+    //Function that will make a call to the Forum API discussion/discussionid endpoint
+  //to retrieve a discussion with the given discussionid
+  getCurrentDiscussion(discussionID: string){
+    return this.http.get<Discussion>( this.forumsUrl + "forum/discussion/" + discussionID);
+  }
+
   getTopics() {
     return this.http.get<string[]>(this.forumsUrl + "forum/topics");
   }
+
+    //Function that will make a call to the Forum API endpoint to add a given topic to a given discussion
+    addTopicToDiscussion(discussionid: string, topicid: string)
+    {
+      return this.http.post( this.forumsUrl + "forum/discussion/topic/" + discussionid + "/" + topicid, null);
+    }
 
   //Function that will make a call to the Forum API endpoint to filter discussion by topic
   filterDiscussionByTopic(topicid: string){
@@ -40,6 +52,19 @@ export class ForumService {
   getUserComments(userId: string) {
     return this.http.get<Comment[]>(this.forumsUrl + "forum/comments/" + userId);
   }
+
+  //Function that will make a call to the Forum API comments/discussionid endpoint
+  //to retrieve a list of comments associated with given discussionid
+  getDiscussionCommentsPage(discussionID:string, page: number, sortingOrder: string){
+    return this.http.get<Comment[]>( this.forumsUrl + "forum/comments/" + discussionID+ "/" + page + "/" + sortingOrder);
+  }
+
+    //Function that will make a call to the Forum API comments/discussionid endpoint
+  //to retrieve a list of comments associated with given discussionid
+  getDiscussionComments(discussionID:string) {
+    return this.http.get<Comment[]>( this.forumsUrl + "forum/comments/" + discussionID);
+  }
+  
 
   postComment(newComment: any) {
     return this.http.post(this.forumsUrl + "forum/comment", newComment);
