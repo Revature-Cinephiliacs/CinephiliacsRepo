291c95de43a6bff710eab94c5cb11e328316f1c4
Catherine Cheatle <48526094+clcheatle@users.noreply.github.com>
2021-05-04 20:55:09 -0400

    updated search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -60,7 +60,7 @@ export class AppComponent {
   onSubmit() {
     if (this.headerSearch.get('headSearch')!.value != "") {
       let searchParam = JSON.stringify(this.headerSearch.get('headSearch')!.value).substring(1, JSON.stringify(this.headerSearch.get('headSearch')!.value).length - 1);
-      this.router.navigate(["/list/" + searchParam + "/1"]);
+      window.location.href = "/list/" + searchParam + "/1";
     }
   }
 
