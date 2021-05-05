fe203a8a9b686671308f8427890a0ebb85058a1c
Noureldin Kamel <noureldinashraf6@gmail.com>
2021-05-06 00:06:07 +0300

    removes logs

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -8,12 +8,8 @@ export class LoggerService {
   constructor() { }
 
   log(subject: string, info: any) {
-    console.log("", new Date() + ": " + subject + ":");
-    console.log(info);
   }
 
   error(subject: string, info: any) {
-    console.error(new Date() + ": " + subject + ":");
-    console.error(info);
   }
 }
